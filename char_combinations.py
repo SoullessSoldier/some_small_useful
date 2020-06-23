8efb7014045e9742b35eb0e8b6da5c5161f6bcc4
SoullessSoldier <48070769+SoullessSoldier@users.noreply.github.com>
2020-06-23 22:39:33 +0300

    Update char_combinations.py

--- char_combinations.py
+++ char_combinations.py
@@ -1,5 +1,9 @@
 import itertools
 a="quick"
+'''
+У permutations 2 параметра - откуда брать и по сколько символов группировать, т.е пример ниже переберет все перестановки целиком quick, quikc, qucik и тд,
+а если permutations(a,2), то будут группы по 2 символа - qu, qi, qc и тд
+'''
 for i in list(itertools.permutations(a)):
 	print(''.join(i))
     
